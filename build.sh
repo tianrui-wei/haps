7e3024ed6def4d00ff37e5851fe255d37ae8e244
Tianrui Wei <tianrui-wei@outlook.com>
2021-06-05 01:50:28 +0800

    [feat] update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd build/
-sims -sys=manycore -ariane  -vcs_build  -vcs_build_args="-full64 -debug_pp -kdb -lca -LDFLAGS -rdynamic -P ${VERDI_HOME}/share/PLI/VCS/LINUX64/novas.tab ${VERDI_HOME}/share/PLI/VCS/LINUX64/pli.a"
-sims -sys=manycore -ariane  -vcs_run hello_world.c -x_tiles=1 -y_tiles=1  -vcs_build_args="-full64" -sim_run_args="-gui=verdi"
+sims -sys=manycore -ariane  -vcs_build -x_tiles=2 -y_tiles=3 -rtl_timeout=10000000000000000  -vcs_build_args="-full64 -debug_pp -kdb -lca -LDFLAGS -rdynamic -P ${VERDI_HOME}/share/PLI/VCS/LINUX64/novas.tab ${VERDI_HOME}/share/PLI/VCS/LINUX64/pli.a"
+sims -sys=manycore -ariane  -vcs_run hello_world_many.c -x_tiles=2 -y_tiles=3 -rtl_timeout=10000000000000000  -vcs_build_args="-full64" -sim_run_args="-gui=verdi"
